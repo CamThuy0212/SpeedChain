2b3b96b619f5354a683fef35c942da36085de02f
CamThuy0212 <camthuy0212@gmail.com>
2024-08-29 16:00:01 +0700

    add speeder screen

--- src/screens/Speeder/Speeder.tsx
+++ src/screens/Speeder/Speeder.tsx
@@ -1,4 +1,13 @@
-import { View, Text, StyleSheet, ImageBackground, Animated, Dimensions, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Animated,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import React, { useEffect, useRef } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -7,12 +16,28 @@ const { width, height } = Dimensions.get('window');
 
 const Speeder = () => {
   const translateX = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(1)).current; // Giá trị Animated cho việc thu nhỏ hình ảnh thứ hai
+
+  const handlePress = () => {
+    Animated.sequence([
+      Animated.timing(scaleAnim, {
+        toValue: 0.9, // Thu nhỏ hình ảnh xuống 90% kích thước ban đầu
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scaleAnim, {
+        toValue: 1, // Trở lại kích thước ban đầu
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   useEffect(() => {
     Animated.loop(
       Animated.sequence([
         Animated.timing(translateX, {
-          toValue: 50, // Di chuyển sang phải 200 đơn vị
+          toValue: 20, // Di chuyển sang phải 200 đơn vị
           duration: 2000, // Thời gian di chuyển là 1 giây
           useNativeDriver: true,
         }),
@@ -34,14 +59,31 @@ const Speeder = () => {
         <Header />
         <View style={styles.container1}>
           <View style={styles.container2}>
-            <Animated.View style={[styles.pedestal, { transform: [{ translateX }] }]}>
+            <Animated.View
+              style={[styles.pedestal, { transform: [{ translateX }] }]}>
               <Image
                 resizeMode="contain"
                 source={require('../../assets/images/ig-pedestal.png')}
                 style={styles.pedestalIg}
               />
             </Animated.View>
-
+            <TouchableOpacity
+              style={styles.missile}
+              onPress={handlePress}
+              activeOpacity={1}>
+              <Animated.Image
+                resizeMode="contain"
+                source={require('../../assets/images/ig-missile.png')}
+                style={[styles.missileIg, { transform: [{ scale: scaleAnim }] }]}
+              />
+            </TouchableOpacity>
+            <View style={styles.wind}>
+              <Image
+                resizeMode="contain"
+                source={require('../../assets/images/ig-wind.png')}
+                style={styles.windIg}
+              />
+            </View>
           </View>
         </View>
         <Footer />
@@ -55,12 +97,12 @@ export default Speeder;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
   },
   backgroundImage: {
     flex: 1,
     width: '100%',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   container1: {
     flex: 0.65,
@@ -70,15 +112,45 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 10,
     justifyContent: 'flex-end',
-    // borderWidth: 1,
+    // alignItems: 'center',
   },
   pedestal: {
-    marginLeft: -25,
-    flex: height > 800 ? 0.4 : 0.43,
+    flex: 1,
+    marginLeft: -10,
     width: '100%',
+    height: '100%',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
   },
   pedestalIg: {
+    flex: 0.4,
+    width: '100%',
+    height: '100%',
+  },
+  missile: {
     flex: 1,
     width: '100%',
+    height: '100%',
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  missileIg: {
+    flex: 1,
+    width: '75%',
+  },
+  wind: {
+    flex: 1,
+    width: '100%',
+    height: '75%',
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    pointerEvents: 'none',
+  },
+  windIg: {
+    flex: 1,
+    width: '40%',
   },
 });
