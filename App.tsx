fc62fe65b1d9d3b893ce17a7d0c6ffbfcf761a91
CamThuy0212 <camthuy0212@gmail.com>
2024-09-09 16:19:22 +0700

    lock screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
-import HomeScreen from './src/screens/Homes/HomeScreen'
+import { StatusBar } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/routers/Router';
 
 const App = () => {
   return (
     <>
-      <StatusBar translucent barStyle={'dark-content'} backgroundColor='transparent'></StatusBar>
+      <StatusBar
+        translucent
+        barStyle={'dark-content'}
+        backgroundColor="transparent"></StatusBar>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
     </>
-  )
+  );
 };
 
 export default App;
